b5c98992eafa7931a493820351547184a14148c3
dhika1416@gmail.com <dhika1416@gmail.com>
2017-01-31 14:30:28 +0700

    nilai maks

---
+++ maxx1.java
@@ -0,0 +1,17 @@
+class maks {
+public static void main(String []args){
+    int nmber1= 65;
+    int nmber2= 75;
+    int nmber3= 45;
+    int maxx = 0;
+    
+    if(nmber1 > maxx)
+        maxx = nmber1;
+    if(nmber2 > maxx)
+        maxx = nmber2;
+    if (nmber3 > maxx)
+        maxx = nmber3;   
+System.out.println("nilai Max dari angka 65,75,45 adalah "+maxx);
+    
+}
+}
